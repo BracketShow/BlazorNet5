9e9a2c1be0b7e91f9fa9f843b3ff03989ef07ccc
ThaRaven403 <bruno.barrette@gmail.com>
2020-10-29 01:03:20 +0000

    Switched pages to use partial classes

---
+++ src/BlazorNet5/Client/Pages/FetchData.razor.cs
@@ -0,0 +1,21 @@
+﻿using BlazorNet5.Shared;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace BlazorNet5.Client.Pages
+{
+    public partial class FetchData
+    {
+        private WeatherForecast[] forecasts;
+
+        [Inject]
+        public HttpClient Http { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("WeatherForecast");
+        }
+    }
+}
---
+++ src/BlazorNet5/Client/Shared/SurveyPrompt.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorNet5.Client.Shared
+{
+    public partial class SurveyPrompt
+    {
+        [Parameter]
+        public string Title { get; set; }
+    }
+}
